1b42b2480064a9fabfce1c1976bbf42843b42364
EduardKorkuts <Eduard.Korkuts@gmail.com>
2022-11-06 01:17:11 +0300

    feat: made a draft of home page

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.png']
+  },
+
+  devServer: {
+    port: 9999
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          'file-loader'
+        ],
+          include: path.join(__dirname, 'assets')
+      }
+    ]
+  }
+}
